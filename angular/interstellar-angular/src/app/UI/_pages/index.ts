59a9ea7deb1f1fbd04ead7d45f54825695f7d4a1
tyler <tcitrin@indiana.edu>
2018-02-27 09:30:58 -0500

    slow restructure: thought considerations on modularization, overlap, injection

---
+++ angular/interstellar-angular/src/app/UI/_pages/index.ts
@@ -0,0 +1,4 @@
+export * from './profile/profile.component';
+export * from './product-page/product-page.component';
+export * from './register/register.component';
+export * from './welcome/welcome.component';
--- angular/interstellar-angular/src/app/UI/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './components/dialog/confirm.dialog.component';
-
-export * from './navbar/navbar.component';
-export * from './profile/profile.component';
-export * from './product/product.component';
-export * from './register/register.component';
-export * from './welcome/welcome.component';
