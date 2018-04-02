8e1a7b83fa40bcb27df349ec0916c1ec846bb4dc
Jon Anderson <jma7@indiana.edu>
2018-04-02 14:20:26 -0400

    merging current html layout

--- angular/interstellar-angular/src/app/marketplace/category/category-feed/category-feed.component.ts
+++ angular/interstellar-angular/src/app/marketplace/category/category-feed/category-feed.component.ts
@@ -8,6 +8,12 @@ export const categories: ProductCategory[] = [
   new ProductCategory('Food', 'good eats', 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/food-drink/2018/02/06/fast-food-restaurants-may-be-healthier-than-five-star-restaurants/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1517968966228.jpg?ve=1&tl=1'),
   new ProductCategory('Electronics', 'get all your tech faves','https://www.santabarbaraloan.com/wp-content/uploads/2017/04/electronicswebuy.jpg'),
   new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+  new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+  new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+  new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+  new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+  new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+  new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
 ];
 
 @Component({
