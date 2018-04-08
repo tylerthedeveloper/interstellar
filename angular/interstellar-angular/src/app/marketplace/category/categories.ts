335e3eb1a8f2a0c2b2bb47031aa2ed45d83d9289
Jon Anderson <jma7@indiana.edu>
2018-04-07 23:17:45 -0400

    UI on category page and checkout. Next is profile

--- angular/interstellar-angular/src/app/marketplace/category/categories.ts
+++ angular/interstellar-angular/src/app/marketplace/category/categories.ts
@@ -1,21 +1,16 @@
 import { ProductCategory } from 'app/marketplace/_market-models/product-category';
 
 export const categories: ProductCategory[] = [
-    new ProductCategory('Food',
-    'good eats',
-    'http://a57.foxnews.com/images.foxnews.com/content/fox-news/' +
-    'food-drink/2018/02/06/fast-food-restaurants-may-be-healthier-than-five-star-restaurants/' +
-    '_jcr_content/par/featured_image/media-0.img.jpg/931/524/1517968966228.jpg?ve=1&tl=1'),
+    new ProductCategory('Food & Grocery','For when you need fuel.','https://images.pexels.com/photos/459469/pexels-photo-459469.jpeg?cs=srgb&dl=basil-delicious-food-459469.jpg&fm=jpg'),
 
     new ProductCategory('Electronics',
-                        'get all your tech faves',
-                        'https://www.santabarbaraloan.com/wp-content/uploads/2017/04/electronicswebuy.jpg'),
+                        'For work or play.',
+                        'https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?cs=srgb&dl=apple-business-computer-39284.jpg&fm=jpg'),
 
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
-    new ProductCategory('Sports', 'stay athletic and active', 'http://urbanmediatoday.com/wp-content/uploads/2016/08/sports.jpg'),
+    new ProductCategory('Sports & Outdoors', 'For your next adventure.', 'https://images.pexels.com/photos/364308/pexels-photo-364308.jpeg?cs=srgb&dl=ball-fun-game-364308.jpg&fm=jpg'),
+    new ProductCategory('Books & Audio', 'For your next rainy day.', 'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?cs=srgb&dl=bookcase-books-bookshelves-159711.jpg&fm=jpg'),
+    new ProductCategory('Movies, Music & Games', 'For time with friends.', 'https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?cs=srgb&dl=cinema-food-movie-theater-33129.jpg&fm=jpg'),
+    new ProductCategory('Clothing, Shoes & Jewelry, ', 'For that next night out.', 'https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?cs=srgb&dl=accessories-accessory-bag-322207.jpg&fm=jpg'),
+    new ProductCategory('Beauty & Health', 'For you.', 'https://images.pexels.com/photos/6148/brush-makeup-make-up-brushes.jpg?cs=srgb&dl=brushes-make-up-makeup-6148.jpg&fm=jpg'),
+    new ProductCategory('Home, Garden, Pets & Tools', 'For where you call home.', 'https://images.pexels.com/photos/597329/pexels-photo-597329.jpeg?cs=srgb&dl=art-materials-arts-and-crafts-bloom-597329.jpg&fm=jpg'),
   ];
